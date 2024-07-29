aa3bc08ef961fe08d1e0605f16f26e05d3013835
Krish <prathamrajsri2003@gmail.com>
2024-07-29 18:09:09 +0530

    final touch

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 
 export default function Footer() {
+    const date = new Date();
+    const year = date.getFullYear()
     return (
-        <footer>&#169; 2023 #VANLIFE</footer>
+        <footer>&#169; {year} #VANLIFE</footer>
     )
 }
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,10 +3,16 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
     return (
-        <div className="home-container">
-            <h1>You got the travel plans, we got the travel vans.</h1>
-            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-            <Link to="vans">Find your van</Link>
-        </div>
+       <div className="container-hm">
+            <div className="home-container">
+                <h1>You got the travel plans, we got the travel vans.</h1> 
+            <div className="container2">
+                <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            </div>
+            <div className="vans">
+                <Link to="vans">Find your van</Link>
+            </div>
+            </div>
+       </div>
     )
 };
